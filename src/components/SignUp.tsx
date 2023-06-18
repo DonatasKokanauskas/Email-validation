5bb7a0399533c84e90a434c34483be06b5dcb483
DonatasKokanauskas <125907210+DonatasKokanauskas@users.noreply.github.com>
2023-06-18 12:32:52 +0300

    final

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,23 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "../style/css/SignUp.css";
-import axios from "axios";
 import SocialLinks from "./SocialLinks";
-import cover from "../images/cover.jpg";
 
 const SignUp = () => {
   const [emailField, setEmailField] = useState<HTMLInputElement>();
   const [emailLabel, setEmailLabel] = useState<HTMLLabelElement>();
   const [emailMessage, setEmailMessage] = useState<HTMLParagraphElement>();
   const [lastEmail, setLastEmail] = useState<string>("");
-  const [ip, setIP] = useState<string>("");
-  const [apiData, setApiData] = useState<
-    { email: string; ipAddress: string }[]
-  >([]);
-
-  const getData = async () => {
-    const res = await axios.get("https://api.ipify.org?format=json");
-    setIP(res.data.ip);
-  };
 
   useEffect(() => {
     setEmailField(document.getElementById("email-field") as HTMLInputElement);
@@ -25,36 +14,15 @@ const SignUp = () => {
     setEmailMessage(
       document.getElementById("email-message") as HTMLParagraphElement
     );
-    getData();
+    setLastEmail(() => {
+      const localStorageEmail = localStorage.getItem("lastEmail") as string;
+      if (localStorageEmail) {
+        return localStorageEmail;
+      } else {
+        return "";
+      }
+    });
   }, []);
-
-  const fetchData = async () => {
-    try {
-      const res = await axios.get(
-        "https://api.jsonbin.io/v3/b/647de7488e4aa6225ea98f9f",
-        {
-          headers: {
-            "X-Master-Key":
-              "$2b$10$3LJ.NgDg5gXwRup9ken64eh7/RcMxrHtMKbKHeR0fCYlXHmcVMA8u",
-          },
-        }
-      );
-
-      res.data.record.forEach((obj: { ipAddress: string; email: string }) => {
-        if (obj.ipAddress === ip && obj.ipAddress !== "") {
-          setLastEmail(obj.email);
-        }
-      });
-
-      setApiData(res.data.record);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [ip]);
 
   useEffect(() => {
     if (emailField) {
@@ -118,43 +86,18 @@ const SignUp = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    try {
-      await axios.put(
-        "https://api.jsonbin.io/v3/b/647de7488e4aa6225ea98f9f",
-        apiData,
-        {
-          headers: {
-            "content-type": "application/json",
-            "X-Master-Key":
-              "$2b$10$3LJ.NgDg5gXwRup9ken64eh7/RcMxrHtMKbKHeR0fCYlXHmcVMA8u",
-          },
-        }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleClick = () => {
     const formElement = document.querySelector("form") as HTMLFormElement;
     const data = new FormData(formElement);
     const formObject = Object.fromEntries(data.entries());
     const email = String(formObject.email);
+
     if (
       emailField?.value.match(
         /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/
       ) &&
       emailField?.value !== ""
     ) {
-      setApiData(() => {
-        return [
-          ...apiData.filter(
-            (obj) => obj.ipAddress !== ip && obj.ipAddress !== ""
-          ),
-          { email: email, ipAddress: ip },
-        ];
-      });
+      localStorage.setItem("lastEmail", email);
     }
   };
 
@@ -174,7 +117,7 @@ const SignUp = () => {
             spellCheck="false"
             onKeyUp={handleKeyUp}
           />
-          <button onClick={handleClick}>Get started</button>
+          <button>Get started</button>
           <p id="email-message"></p>
         </form>
       </div>
